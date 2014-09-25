fd88d5a42d4b5051a7f6d00fc6adc4c7b0f58971
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-09-25 22:00:18 +0100

    Parses input

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"github.com/docopt/docopt-go"
 )
 
-func main() {
+func getArgs() string {
 	usage := `Water.
 
 	Usage:
-	  water <file>
+	  water <values>
 	  water -h | --help
 	  water --version
 
@@ -17,6 +19,30 @@ func main() {
 	  -h --help     Show this screen.
 	  --version     Show version.`
 
-	arguments, _ := docopt.Parse(usage, nil, true, "Water 0.0", false)
-	fmt.Println(arguments)
+	arguments, err := docopt.Parse(usage, nil, true, "Water 0.0", false)
+	if err != nil {
+		panic(err)
+	}
+
+	return arguments["<values>"].(string)
+}
+
+func getIntVals(args string) []int {
+	var intvals []int
+	strvals := strings.Split(args, ",")
+	for i := range strvals {
+		val, err := strconv.Atoi(strvals[i])
+		if err != nil {
+			panic(err)
+		}
+
+		intvals = append(intvals, val)
+	}
+
+	return intvals
+}
+
+func main() {
+	vals := getIntVals(getArgs())
+	fmt.Println(vals)
 }
